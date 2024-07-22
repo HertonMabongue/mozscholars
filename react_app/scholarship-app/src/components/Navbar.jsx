da6b24d096f8b067127e12e3f10dc5933acad9c8
Herton Cabral Mabongue <HERTON@APPLEs-MacBook-Pro.local>
2024-07-22 22:48:31 +0200

    Update website with new changes

--- react_app/scholarship-app/src/components/Navbar.jsx
+++ react_app/scholarship-app/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 
 import logo from '../assets/logo.png';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import React, {  useState } from 'react'; 
+
 
 
 
@@ -11,7 +13,19 @@ const logoStyle = {
 
 
 const Navbar = () => {
-  
+
+  const [isCollapsed, setIsCollapsed] = useState(true); // Initial state: not collapsed
+
+  const handleLinkClick = () => {
+    setIsCollapsed(!isCollapsed); // Toggle collapsed state on click
+  };
+
+  const myDivClassName = isCollapsed
+    ? 'collapse navbar-collapse' // Collapsed state
+    : 'navbar-collapse collapse show'; // Expanded state
+
+
+
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top py-0"
@@ -28,28 +42,27 @@ const Navbar = () => {
           data-bs-toggle="collapse"
           data-bs-target=".navbar-collapse"
           aria-controls="navbarNav"
-          aria-expanded="false"
           aria-label="Toggle navigation"
-         
+          onClick={handleLinkClick} 
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={myDivClassName} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item" >
-              <a className="nav-link"  href="/">Início</a>
+              <a className="nav-link"  href="/" >Início</a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/Sobre" onClick="history.go(0);">Sobre</Link>
+              <Link className="nav-link" to="/Sobre" onClick={handleLinkClick}>Sobre</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="mailto:mozscholars@gmail.com">Contacto</a>
+              <a className="nav-link" href="mailto:mozscholars@gmail.com" onClick={handleLinkClick}>Contacto</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="https://www.ibe.gov.mz/?cat=52" target="_blank" rel="noopener noreferrer">Bolsas Oferecidas pelo IBE</a>
+              <a className="nav-link" href="https://www.ibe.gov.mz/?cat=52" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>Bolsas Oferecidas pelo IBE</a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/Links-Úteis" onClick="history.go(0);">Links Úteis</Link>   
+              <Link className="nav-link" to="/Links-Úteis" onClick={handleLinkClick}>Links Úteis</Link>   
             </li>         
           </ul>
         </div>
